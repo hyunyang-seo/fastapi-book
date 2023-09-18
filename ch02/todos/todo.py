aa2872ef6af80e79b652d663257864b981ee3d51
jadeitefirst <hyunyang.seo@gmail.com>
2023-09-18 18:43:25 +0900

    ch2.4 add & test request and query parameter

--- ch02/todos/todo.py
+++ ch02/todos/todo.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Path
 from model import Todo
 
 todo_router = APIRouter()
@@ -18,4 +18,13 @@ async def retrieve_todos() -> dict:
         "todos": todo_list
     }
     
-
+@todo_router.get("/todo/{todo_id}")
+async def get_single_todo(todo_id: int) -> dict:
+    for todo in todo_list:
+        if todo.id == todo_id:
+            return {
+                "todo": todo
+            }
+    return {
+        "message": "Todo with given id not found."
+    }
